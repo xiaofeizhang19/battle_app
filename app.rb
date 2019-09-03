c17b9e9799b814749e4f62dab0dc5aecfc068885
Neil Campbell <neildcampbell@hotmail.com>
2019-09-03 16:08:05 +0100

    added form

--- app.rb
+++ app.rb
@@ -2,8 +2,16 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
   get '/' do
-    'Testing infrastructure working!'
+    erb(:index)
   end
-  run! if app_file == $0
-end
 
+  post '/names' do
+    @player_1 =params[:player_1]
+    @player_2 =params[:player_2]
+    erb(:play)
+  end
+
+  run! if app_file ==$0
+
+
+end
