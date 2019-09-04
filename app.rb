4819f30cbf1dd07d9a21d8d22e78783cbf7c4031
Ben <ben@benzeng.co.uk>
2019-09-04 14:43:55 +0100

    player logic initiated

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra/base'
+require './lib/player'
 
 class Battle < Sinatra::Base
   enable :sessions
@@ -8,8 +9,10 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    session[:player_1] = params[:player_1]
-    session[:player_2] = params[:player_2]
+    $player_1 = Player.new(params[:player_1])
+    $player_2 = Player.new(params[:player_2])
+    #session[:player_1] = params[:player_1]
+    #session[:player_2] = params[:player_2]
     redirect '/play'
   end
 
@@ -18,13 +21,13 @@ class Battle < Sinatra::Base
   end
 
   get '/play' do
-    @player_1 =  session[:player_1]
-    @player_2 =  session[:player_2]
+    @player_1 =  $player_1.name
+    @player_2 =  $player_2.name
     erb(:play)
   end
 
   get '/attack' do
-    "Marley has been attacked"
+    "<h1>Marley has been attacked</h1>"
   end
 
   run! if app_file == $0
